2841dc6294e730dcc7bec7c4740ff54b52de34d9
Saferer <james.nguy@shaw.ca>
2020-02-26 16:53:38 -0700

    Firebase test

--- APIGateway/routes/feedback.js
+++ APIGateway/routes/feedback.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/test", async (req, res) => {
+    try {
+        fetch('10.13.110.106:4000/feedback/test')
+            .then(res => res.json())
+            .then(body => console.log(body));
+    }
+    catch (e) {
+
+    }
+});
+
+module.exports = router;
