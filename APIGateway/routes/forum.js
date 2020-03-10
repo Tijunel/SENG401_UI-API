ca69cad8cbd3f67a73a97ce5e84711f0d7f52847
Jeremy Bilic <jeremy.bilic@gmail.com>
2020-03-10 14:27:47 -0600

    Added endpoints for feedback

--- APIGateway/routes/forum.js
+++ APIGateway/routes/forum.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const IP = require("../config/connections")
+const http = require("http")
 
-options = {
-    host: IP.forumServiceIP,
-    port: IP.forumServicePort,
-    path: '/',
-    method: 'GET',
-};
 
-router.get('/', async (req, res) => {
+
+router.get('/test', async (req, res) => {
+    let options = {
+        host: '10.13.60.26',
+        port: 3000,
+        path: '/',
+        method: 'GET',
+    };
     http.request(options, (httpResp) => {
         httpResp.on('data', (data) => {
             res.send(JSON.parse(data));
