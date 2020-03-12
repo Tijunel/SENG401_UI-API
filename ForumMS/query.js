1f5f7cf81c6771a07d3bd316c033a9c4b04d2f06
Jeremy Bilic <jeremy.bilic@gmail.com>
2020-03-12 13:23:52 -0600

    Started adding redis read connections

--- ForumMS/query.js
+++ ForumMS/query.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const redis = require('redis')
+const client = redis.createClient({
+    port: 6379,
+    host: 'localhost'
+})
+
+client.on('connect', () => {
+    console.log('Redis client connected')
+})
+
+client.on('error', (err) => {
+    console.log('Redis client could NOT connect: \n' + err)
+})
+
+
+client.get('test key', (err, res) => {
+    if (err) {
+        console.log("could not fetch key")
+    }
+    console.log(res)
+})
 
 router.get("/getTopic/:id", (req, res) => {
+
     // retirieve data using req.params.id as param to search DB
     let readData = JSON.stringify({
         // values will be fetched from database
@@ -15,6 +37,38 @@ router.get("/getTopic/:id", (req, res) => {
 });
 
 router.get("/getForum/:id", (req, res) => {
+    let forum = {
+        forumName: "",
+        topics: []
+    }
+    client.get("name-" + req.params.id, (err, results) => {
+        if (err) {
+            res.status(404).send("Could not find forum")
+            return
+        }
+        forum.forumName = results
+
+        client.get(req.params.id, (err, results) => {
+            if (err) {
+                res.status(404).send("Could not find forum")
+                return
+            }
+
+
+            for (topicID of results) {
+                client.get("name-" + topicID, (err, results) => {
+                    if (err) {
+                        res.status(500).send("Error finding topics in Redis")
+                        return
+                    }
+
+                    forum.topics.push(results)
+                })
+            }
+        })
+    })
+
+
     // retirieve data using req.params.id as param to search DB
     let readData = JSON.stringify({
         // values will be fetched from database
